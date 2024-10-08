4adeb36f1dbe52e06192dbcb7821c4681831f723
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-10-08 20:25:44 +0700

    Feat: get all farms

---
+++ Koi-Web-BE/UseCases/UC_Farms/Queries/GetFarms.cs
@@ -0,0 +1,102 @@
+using Koi_Web_BE.Database;
+using Koi_Web_BE.Endpoints.Internal;
+using Koi_Web_BE.Models.Entities;
+using Koi_Web_BE.Models.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Koi_Web_BE.UseCases.UC_Farms.Queries;
+
+public class GetFarms
+{
+    public record Query(
+        int PageIndex,
+        int PageSize,
+        bool AscByRating,
+        string Name
+    ) : IRequest<Result<Response>>;
+
+    public record FarmResponse(
+        string Name,
+        string Owner,
+        string Address,
+        string Description,
+        decimal Rating,
+        IEnumerable<string> FarmImages
+    )
+    {
+
+        public static FarmResponse FromEntity(Farm farm)
+            => new(
+                Name: farm.Name,
+                Owner: farm.Owner,
+                Address: farm.Address,
+                Description: farm.Description,
+                Rating: farm.Rating,
+                FarmImages: farm.FarmImages.Select(farmImage => farmImage.Url)
+            );
+    }
+
+    public record Response(
+        int PageIndex,
+        int PageSize,
+        int TotalPages,
+        IEnumerable<FarmResponse> Farms
+    );
+
+    public class Handler(IApplicationDbContext context) : IRequestHandler<Query, Result<Response>>
+    {
+        public async Task<Result<Response>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            IQueryable<Farm> query = context.Farms
+                .AsNoTracking()
+                .Include(f => f.FarmKois)
+                .Where(f => f.Name.Contains(request.Name));
+            //calculate total pages
+            var totalFarms = await query.CountAsync(cancellationToken);
+            var totalPages = (int)Math.Ceiling(totalFarms / (double)request.PageSize);
+
+            //sort
+            if (request.AscByRating)
+                query = query.OrderBy(f => f.Rating);
+            else
+                query = query.OrderByDescending(f => f.Rating);
+            //map to response detail
+            IEnumerable<FarmResponse> farms = await query
+                .Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(f => FarmResponse.FromEntity(f))
+                .ToListAsync(cancellationToken);
+            //return response
+            return Result<Response>.Succeed(new Response(
+                PageIndex: request.PageIndex,
+                PageSize: request.PageSize,
+                TotalPages: totalPages,
+                Farms: farms
+            ));
+        }
+    }
+
+    public class Endpoint : IEndpoints
+    {
+        public static void DefineEndpoints(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/farms", Handle)
+                .WithTags("Farms")
+                .WithMetadata(new SwaggerOperationAttribute("Get all Farms"))
+                .RequireAuthorization();
+        }
+
+        public static async Task<IResult> Handle(ISender sender,
+            int pageIndex = 1,
+            int pageSize = 10,
+            bool ascByRating = false,
+            string search = "",
+            CancellationToken cancellationToken = default)
+        {
+            Result<Response> response = await sender.Send(new Query(pageIndex, pageSize, ascByRating, search), cancellationToken);
+            return Results.Ok(response);
+        }
+    }
+}
