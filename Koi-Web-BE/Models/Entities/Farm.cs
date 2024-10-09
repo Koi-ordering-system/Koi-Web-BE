2eee144f17be21ba7d002516c3aebc1ebb95db56
PersieDaCoder <thinhdpham2510@gmail.com>
2024-10-09 11:22:30 +0700

    fix:database

--- Koi-Web-BE/Models/Entities/Farm.cs
+++ Koi-Web-BE/Models/Entities/Farm.cs
@@ -14,5 +14,4 @@ public class Farm : BaseEntity
     public IList<FarmImage> FarmImages { get; set; } = [];
     public IList<Review> Reviews { get; set; } = [];
     public IList<Order> Orders { get; set; } = [];
-    public IList<FarmImage> FarmImages { get; set; } = [];
 }
