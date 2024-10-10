ff0f341ad881022c3c5b45564d143c4c82332150
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-10-10 11:27:25 +0700

    Feat: delete farm

---
+++ Koi-Web-BE/UseCases/UC_Farms/Commands/DeleteFarm.cs
@@ -0,0 +1,66 @@
+using Koi_Web_BE.Database;
+using Koi_Web_BE.Endpoints.Internal;
+using Koi_Web_BE.Exceptions;
+using Koi_Web_BE.Models.Entities;
+using Koi_Web_BE.Models.Primitives;
+using Koi_Web_BE.Utils;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace Koi_Web_BE.UseCases.UC_Farms.Commands;
+
+public class DeleteFarm
+{
+    public record Command(Guid Id) : IRequest<Result<Response>>;
+
+    public record Response();
+
+    public class Handler(IApplicationDbContext context, CurrentUser currentUser, IImageService imageService) : IRequestHandler<Command, Result<Response>>
+    {
+        public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            //check if user has permission to delete
+            if (!currentUser.User!.IsAdmin())
+                return Result<Response>.Fail(new ForbiddenException("The current user is not an admin"));
+            // Get the farm entity
+            Farm? deletingFarm = await context.Farms
+                .Include(farm => farm.FarmImages)
+                .SingleOrDefaultAsync(f => f.Id == request.Id, cancellationToken);
+            if (deletingFarm == null)
+                return Result<Response>.Fail(new NotFoundException("Farm not found"));
+            // Remove farm images in database
+            context.FarmImages.RemoveRange(deletingFarm.FarmImages.ToList());
+            // Remove farm images in cloudinary
+            var deleteTasks = deletingFarm.FarmImages
+                .Select(farmImage => imageService.DeleteImageAsync(farmImage.Url)).ToList();
+            await Task.WhenAll(deleteTasks);
+            if (deleteTasks.Any(task => !task.Result))
+                return Result<Response>.Fail(new InvalidOperationException("Failed to delete images from Cloudinary"));
+            // Remove the farm entity
+            context.Farms.Remove(deletingFarm);
+            await context.SaveChangesAsync(cancellationToken);
+            return Result<Response>.Succeed(null!);
+        }
+    }
+
+    public class Endpoint : IEndpoints
+    {
+        public static void DefineEndpoints(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("api/farms/{id}", Handle)
+            .WithTags("Farms")
+            .WithMetadata(new SwaggerOperationAttribute("Delete a Farm"))
+            .RequireAuthorization();
+        }
+
+        public static async Task<IResult> Handle(ISender sender, Guid id, CancellationToken cancellationToken = default)
+        {
+            Result<Response> result = await sender.Send(new Command(id), cancellationToken);
+            if (!result.Succeeded)
+                return Results.BadRequest(result);
+            return Results.NoContent();
+        }
+    }
+}
