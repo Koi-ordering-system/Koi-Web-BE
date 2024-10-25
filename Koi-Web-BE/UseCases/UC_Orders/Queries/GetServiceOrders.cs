2ffff35a7baa9d5a1b44d500be99b9aea01b7da7
PersieDaCoder <thinhdpham2510@gmail.com>
2024-10-25 19:42:09 +0700

    feat/get user role

--- Koi-Web-BE/UseCases/UC_Orders/Queries/GetServiceOrders.cs
+++ Koi-Web-BE/UseCases/UC_Orders/Queries/GetServiceOrders.cs
@@ -34,7 +34,9 @@ public class GetServiceOrders
         string Status,
         DateTimeOffset StartDate,
         DateTimeOffset EndDate,
-        TripStatusEnum TripStatusEnum
+        TripStatusEnum TripStatusEnum,
+        KoiDetail[] Kois,
+        TripDetail? Trip
     )
     {
         public static OrderDetailResponse FromEntity(Order order)
@@ -49,9 +51,43 @@ public class GetServiceOrders
                 Status: order.Status?.ToString() ?? string.Empty,
                 StartDate: order.OrderTrip?.StartDate ?? default,
                 EndDate: order.OrderTrip?.EndDate ?? default,
-                TripStatusEnum: order.OrderTrip?.Status ?? default
+                TripStatusEnum: order.OrderTrip?.Status ?? default,
+                Kois: order.OrderKois.Count == 0 ? [] : order.OrderKois.Select(ok => ok.Koi).Select(k => new KoiDetail
+                {
+                    Id = k.Id,
+                    Name = k.Name,
+                    Description = k.Description,
+                    MinSize = k.MinSize,
+                    MaxSize = k.MaxSize,
+                    Price = k.Price,
+                    ImageUrls = k.Images.Select(i => i.Url).ToArray(),
+                    Colors = k.Colors.Select(c => c.Name).ToArray()
+                }).ToArray(),
+                Trip: order.OrderTrip is null ? null : new(
+                    Id: order.OrderTrip?.Id ?? default,
+                    Days: order.OrderTrip?.Trip?.Days ?? 0,
+                    Price: order.OrderTrip?.Trip?.Price ?? 0
+                )
             );
     }
+
+    public class KoiDetail
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal MinSize { get; set; } = 0;
+        public decimal MaxSize { get; set; } = 0;
+        public decimal Price { get; set; } = 0;
+        public string[] ImageUrls { get; set; } = [];
+        public string[] Colors { get; set; } = [];
+    }
+
+    public record TripDetail(
+        Guid Id,
+        int Days,
+        decimal Price
+    );
 
     public class Handler(IApplicationDbContext context) : IRequestHandler<Query, Result<Response>>
     {
@@ -60,7 +96,8 @@ public class GetServiceOrders
             IQueryable<Order> query = context.Orders
                                             .AsNoTracking()
                                             .Include(o => o.Farm)
-                                            .Include(o => o.OrderTrip);
+                                            .Include(o => o.OrderTrip).ThenInclude(ot => ot.Trip)
+                                            .Include(o => o.OrderKois).ThenInclude(ok => ok.Koi);
             int count = await query.CountAsync(cancellationToken);
             IEnumerable<OrderDetailResponse> orders = await query.Skip((request.PageIndex - 1) * request.PageSize)
                                                                 .Take(request.PageSize)
@@ -82,8 +119,7 @@ public class GetServiceOrders
         {
             app.MapGet("api/orders/service", Handle)
             .WithTags("Orders")
-            .WithMetadata(new SwaggerOperationAttribute("Get Service Orders"))
-            .RequireAuthorization();
+            .WithMetadata(new SwaggerOperationAttribute("Get Service Orders"));
         }
 
         public static async Task<IResult> Handle(ISender sender, int pageIndex = 1, int pageSize = 10, CancellationToken cancellationToken = default)
